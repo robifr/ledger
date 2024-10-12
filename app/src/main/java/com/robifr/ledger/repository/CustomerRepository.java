81b403893290d7a12d190472701131b13647f63d
robi <robifr@users.noreply.github.com>
2024-10-12 20:07:32 +0700

    ref: update data model implementation to adopt the new builder approach

--- app/src/main/java/com/robifr/ledger/repository/CustomerRepository.java
+++ app/src/main/java/com/robifr/ledger/repository/CustomerRepository.java
@@ -241,8 +241,7 @@ public final class CustomerRepository
     final CompletableFuture<BigDecimal> countDebt =
         CompletableFuture.supplyAsync(() -> this._localDao.totalDebtById(customer.id()));
 
-    return countDebt.thenApplyAsync(
-        debt -> CustomerModel.toBuilder(customer).setDebt(debt).build());
+    return countDebt.thenApplyAsync(customer::withDebt);
   }
 
   /**
