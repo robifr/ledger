df5e5ee4dc0f201d7fec367c1fed66479777dd81
robi <robifr@users.noreply.github.com>
2024-12-15 15:13:15 +0700

    chore: bump version to v1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     minSdk = 30
     targetSdk = 35
     versionCode = 1
-    versionName = "1.1.4"
+    versionName = "1.1.5"
     multiDexEnabled = true
     setProperty("archivesBaseName", "ledger-v${versionName}")
 
