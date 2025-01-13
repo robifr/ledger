fb17885e26d9d13f9adb4eb0e8490cec6ffe7b9a
robi <robifr@users.noreply.github.com>
2025-01-13 07:30:50 +0700

    ref: move `ModelSyncListener` and `InfoSyncListener` in one file

--- app/src/main/java/com/robifr/ledger/repository/ModelChangedListener.kt
+++ app/src/main/java/com/robifr/ledger/repository/ModelChangedListener.kt
@@ -16,6 +16,9 @@
 
 package com.robifr.ledger.repository
 
+import com.robifr.ledger.data.InfoSynchronizer
+import com.robifr.ledger.data.ModelSynchronizer
+import com.robifr.ledger.data.model.Info
 import com.robifr.ledger.data.model.Model
 
 interface ModelChangedListener<M : Model> {
@@ -26,4 +29,47 @@ interface ModelChangedListener<M : Model> {
   fun onModelDeleted(models: List<M>)
 
   fun onModelUpserted(models: List<M>)
+}
+
+class ModelSyncListener<M : Model>(
+    val currentModel: () -> List<M>,
+    val onSyncModels: (syncedModels: List<M>) -> Unit
+) : ModelChangedListener<M> {
+  override fun onModelAdded(models: List<M>) {
+    onSyncModels(ModelSynchronizer.addModel(currentModel(), models))
+  }
+
+  override fun onModelUpdated(models: List<M>) {
+    onSyncModels(ModelSynchronizer.updateModel(currentModel(), models))
+  }
+
+  override fun onModelDeleted(models: List<M>) {
+    onSyncModels(ModelSynchronizer.deleteModel(currentModel(), models))
+  }
+
+  override fun onModelUpserted(models: List<M>) {
+    onSyncModels(ModelSynchronizer.upsertModel(currentModel(), models))
+  }
+}
+
+class InfoSyncListener<I : Info, M : Model>(
+    val currentInfo: () -> List<I>,
+    val modelToInfo: (M) -> I,
+    val onSyncInfo: (syncedInfo: List<I>) -> Unit
+) : ModelChangedListener<M> {
+  override fun onModelAdded(models: List<M>) {
+    onSyncInfo(InfoSynchronizer.addInfo(currentInfo(), models, modelToInfo))
+  }
+
+  override fun onModelUpdated(models: List<M>) {
+    onSyncInfo(InfoSynchronizer.updateInfo(currentInfo(), models, modelToInfo))
+  }
+
+  override fun onModelDeleted(models: List<M>) {
+    onSyncInfo(InfoSynchronizer.deleteInfo(currentInfo(), models))
+  }
+
+  override fun onModelUpserted(models: List<M>) {
+    onSyncInfo(InfoSynchronizer.upsertInfo(currentInfo(), models, modelToInfo))
+  }
 }
